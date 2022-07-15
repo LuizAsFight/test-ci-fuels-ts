d05912ce19c4aa478d751cca61cc2519899aa836
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-07-15 15:15:21 -0300

    fix

--- packages/script/scripts/build-call-test-script.sh
+++ packages/script/scripts/build-call-test-script.sh
@@ -2,11 +2,16 @@
 
 set -euo pipefail
 
+echo 1;
 FUELS_DEBUG=${FUELS_DEBUG:-}
 SCRIPT_DIR="src/call-test-script"
 BIN_DIR="$SCRIPT_DIR/out/debug"
 
+echo 2;
 pnpm forc build -p $SCRIPT_DIR --print-finalized-asm
+echo 3;
 if [[ -n "$FUELS_DEBUG" ]]; then
+echo 4;
   pnpm forc parse-bytecode "$BIN_DIR/call-test-script.bin" > "$BIN_DIR/call-test-script.txt"
+  echo 5;
 fi
